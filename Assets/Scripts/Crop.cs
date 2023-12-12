d0dd3e072039e337266eebadf7eb1ad6ea6fd74f
Igor Makarov <firstindustry229@gmail.com>
2023-12-12 19:20:24 +0200

    Assets Library new repository

---
+++ Assets/Scripts/Crop.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crop : MonoBehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] private Transform _croprenderer;
+    [SerializeField] private ParticleSystem _harvestParticles;
+
+    private Vector3 _cornGrownSize = new Vector3(1f, 1f, 1f);
+
+    public void ScaleUp()
+    {
+        _croprenderer.gameObject.LeanScale(_cornGrownSize, 1).setEase(LeanTweenType.easeInBack);
+    }
+    
+    public void ScaleDown()
+    {
+        _croprenderer.gameObject.LeanScale(Vector3.zero, 1).
+            setEase(LeanTweenType.easeInBack).setOnComplete(DestroyCrop);
+
+        _harvestParticles.gameObject.SetActive(true);
+        _harvestParticles.transform.parent = null;
+        _harvestParticles.Play();
+    }
+
+    private void DestroyCrop()
+    {
+        Destroy(gameObject);
+    }
+}
